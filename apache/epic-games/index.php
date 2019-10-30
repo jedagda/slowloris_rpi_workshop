1d94d7dd2719fdc004b3b83ce8dd16dcfc01ab86
Josh Dagda <jedagda@miners.utep.edu>
2019-10-29 18:29:52 -0600

    Commit correction, forgot to remove the previous .git files

---
+++ apache/epic-games/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang = "en-US">
+	<head>
+	<meta charset = "UTF-8">
+	<title>Epic Games</title>
+	<link rel="stylesheet" href="styles.css">
+	</head>
+<body>
+	<img src="epicgames.jpg" class="responsive">
+</body>
+</html>
